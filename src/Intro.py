031fa1c44ee1f6780ce05c82a7aa4046bfc35169
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2024-06-29 11:41:54 +0000

    Update Intro.py

--- src/Intro.py
+++ src/Intro.py
@@ -2,10 +2,8 @@ import tkinter as tk
 top=tk.Tk()
 label=tk.Button(text='''Welcome to JV
 Presented by-
-Vijeta Priya
-Jaya Ambastha
-Co-presented by-
-Shipra Mam''', fg="blue", bg="white", width=50, height=25)
+Vijeta Priya''',
+fg="blue", bg="white", width=50, height=25)
 label.pack()
 window.mainloop()
 window=tk.Tk()
